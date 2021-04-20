a9b3c37b81abb72be6aaa5171b200ab9b57b99a7
alizadeh-mohsen <kelarens@gmail.com>
2021-04-20 13:25:05 +0430

    Formatting index.js

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const mongoose = require('mongoose');
 const movies = require('./routes/Movies');
 
 const express = require('express');
-const { string, boolean, number } = require('joi');
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.json());
+app.use('/api/movies', movies);
+
+
+app.listen(3000, () => {
+    console.log('listening to port 3000....');
+});
+
+app.get('/', (req, res) => {
+    res.render('homepage');
+});
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('connected to mongoDb'))
@@ -33,22 +46,3 @@ async function createMovie() {
 }
 
 createMovie();
-
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-app.use(express.json());
-app.use('/api/movies', movies);
-
-app.get('/', (req, res) => {
-    res.end();
-    res.render('homepage', { title: 'Movies...', target: 'http://localhost:3000/api/movies' })
-});
-
-app.listen(3000, () => {
-    console.log('listening to port 3000....');
-});
-
-app.get('/', (req, res) => {
-    res.render('homepage');
-});
