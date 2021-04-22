c0161a3c64b8c01ac7aed8527c44bccdf7231d6a
alizadeh-mohsen <kelarens@gmail.com>
2021-04-22 16:46:32 +0430

    extracting  models

---
+++ routes/genres.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const {Genre, vlidate} = require('../models/genre');
+
+router.get('/', async (req, res) => {
+    const genres = await Genre
+        .find()
+        .sort('name')
+        .select('name');
+
+    res.send(genres);
+});
+
+router.get('/:id', async (req, res) => {
+
+    const genre = await Genre.findById(req.params.id);
+    if (!genre)
+        return res.status(404).send('Genre not found');
+    res.send(genre);
+});
+
+router.post('/', async (req, res) => {
+    const {error} = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    try {
+
+        let genre = new Genre({
+            name: req.body.name
+        });
+
+        genre = await genre.save();
+        res.send(genre);
+
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {error} = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let genre = await Genre.findByIdAndUpdate(
+        req.params.id,
+        {name: req.body.name},
+        {new: true}
+    );
+
+    if (!genre) return res.status(404).send('Genre not found');
+
+    res.send(genre);
+
+});
+
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+    if (!genre) return res.status(404).send('Genre not found');
+
+    res.send(genre);
+});
+
+module.exports = router;
